aaceef8bb64ee7c82715c91637920a610c56b66f
Ankit Saini <sainiankit@hotmail.com>
2019-07-21 19:40:14 +0530

    Examples Updated

--- src/components/Todo/presentational/Todo.js
+++ src/components/Todo/presentational/Todo.js
@@ -3,10 +3,6 @@ import { Button } from 'antd';
 import { Collapse, Icon } from 'antd';
 const { Panel } = Collapse;
 
-/**
- * TODO:
- * Change this text
- */
 const text = `
   A dog is a type of domesticated animal.
   Known for its loyalty and faithfulness,
@@ -34,6 +30,7 @@ const Todo = (props) => {
                         bordered={false}
                         defaultActiveKey={['1']}
                         expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+                        style={{marginTop: '40px'}}
                 >
                         {props.todoList.map(item => {
                                 return <Panel header={item.title} key={item.id} style={customPanelStyle}>
