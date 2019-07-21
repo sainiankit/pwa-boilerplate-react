fb5ef568de7efb5d00eaba637f1568ca4b6addce
Ankit Saini <sainiankit@hotmail.com>
2019-07-21 18:57:23 +0530

    Descriptions added

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -5,10 +5,20 @@
 
 import { actionTypes } from '../constants';
 
+/**
+ * @name initialState Initial state for state.counter object
+ */
 const initialState = {
         count: 0
 };
 
+/**
+ * Redux Reducer function for state.counter
+ * 
+ * @param {Object} state Current state (state.counter)
+ * @param {Object} action Redux action object
+ * @returns {Object} state return the new state for (state.counter)
+ */
 function reducer(state = initialState, action) {
 
         switch(action.type) {
